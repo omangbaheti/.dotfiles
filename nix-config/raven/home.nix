9e428876a2ee3839739dbda8fe4e2ce6f5e42878
Omang Baheti <omangbaheti@gmail.com>
2026-02-22 10:54:14 +0000

    adding some more packages

--- nix-config/raven/home.nix
+++ nix-config/raven/home.nix
@@ -1,27 +1,27 @@
 { config, pkgs, stable, machine, ... }:
 {
-
   home.stateVersion = "26.05";
+  
   programs.home-manager.enable = true;
-  
   programs.zoxide.enable = true;
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name = "Omang Baheti";
-      user.email = "omangbaheti@gmail.com";
-      init.defaultBranch = "main";
-      push.default = "simple";
+  programs.git = 
+    {
+      enable = true;
+      settings = {
+        user.name = "Omang Baheti";
+        user.email = "omangbaheti@gmail.com";
+        init.defaultBranch = "main";
+        push.default = "simple";
+      };
     };
-  };
-  
+
   programs.zsh = 
     {
       enable = true;
       enableCompletion = true;
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
-  
+
       shellAliases = 
         {
           ll = "eza -l";
@@ -30,39 +30,23 @@
           tree = "eza --tree --icons --ignore-glob='.git|.venv|node_modules'";
           grep = "rg";
           find = "fd";
-          e="emacsclient -c";
+          #          e="emacsclient -c";
           emd = "emacs --daemon";
           rebuild-config = "sudo nixos-rebuild switch --flake ~/.dotfiles/nix-config#${machine.systemType}";
           rebuild-home-config = "home-manager switch --flake  ~/.dotfiles/nix-config#${machine.username}@${machine.host}";
-          exp="/mnt/c/WINDOWS/explorer.exe .";
         };
-  
+
       oh-my-zsh = 
         {
           enable = true;
-          plugins = [ "git" "sudo" ];
-          theme = "robbyrussell";
         };
     };
-  
+
   services.syncthing = 
     {
       enable = true;
     };
-  
-  programs.gpg.enable = true;
-  
-  services.gpg-agent = 
-    {
-      enable = true;
-      pinentry.package = pkgs.pinentry-curses; 
-      extraConfig = ''
-      default-cache-ttl = 31536000;  # 1 year in seconds
-      max-cache-ttl = 31536000;
-        allow-loopback-pinentry
-      '';
-    };  
-  
+
   home.packages = (with pkgs;
     [
       git
@@ -76,7 +60,6 @@
       oh-my-posh
       tealdeer
       btop
-      #nnn  
       fd
       eza
       devenv
@@ -84,9 +67,15 @@
       yazi
       diff-so-fancy
       fastfetch
+      openssh
       ripgrep
       poetry
       pandoc
+      emacs-nox
+      tirith
+      android-tools
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
     ]);
   programs.zsh.initContent = 
     ''
