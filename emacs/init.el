76084ef200b171e7f4074a90f3bf2904324ad7e3
omangbaheti <omangbaheti@gmail.com>
2026-04-08 10:27:17 -0700

    fixing startup times

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,7 @@
 (setq user-emacs-directory "~/.emacs.d/")  ;; writable path for config
 (setq package-user-dir "~/.emacs.d/elpa/") ;; writable path for installed packages
+(add-to-list 'default-frame-alist '(background-color . "#1d1f21"))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; (setq package-enable-at-startup nil)
 (setq evil-want-keybinding nil)
 (setq gc-cons-threshold #x40000000)
