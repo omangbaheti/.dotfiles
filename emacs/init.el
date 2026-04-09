c529e24f17dd4c3504b7b802b9e616c8b309967a
omangbaheti <omangbaheti@gmail.com>
2026-04-09 12:10:53 -0700

    maximise when launched

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,5 @@
 (setq user-emacs-directory "~/.emacs.d/")  ;; writable path for config
 (setq package-user-dir "~/.emacs.d/elpa/") ;; writable path for installed packages
-(add-to-list 'default-frame-alist '(background-color . "#1d1f21"))
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
 ;; (setq package-enable-at-startup nil)
 (setq evil-want-keybinding nil)
 (setq gc-cons-threshold #x40000000)
@@ -10,3 +8,4 @@
  (expand-file-name
   "~/.dotfiles/emacs/config.org"
   user-emacs-directory))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
