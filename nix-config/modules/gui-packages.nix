8a1bd0652ff0f60b048d0d20c6d285981def9ecd
Omang Baheti <omangbaheti@gmail.com>
2026-06-12 02:07:24 -0400

    adding new gui module

---
+++ nix-config/modules/gui-packages.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, stable, machine, inputs, ... }:
+{
+  home.packages = (with pkgs; [
+    #Dev-Tools
+    jetbrains.rider
+    unityhub
+
+    blender
+    rclone
+
+    proton-vpn
+    localsend
+
+    #Tools
+    libreoffice
+    gimp
+    aseprite
+    zotero
+    obsidian
+    stable.android-studio
+    audacity
+
+    #Media
+    vlc
+    obs-studio
+    spotify
+    parsec-bin
+
+    #Communication
+    discord
+    telegram-desktop
+    slack
+    zoom-us
+    steam
+
+    # Virtual Machines and wine
+    libvirt
+    wine
+    virt-manager
+    qemu
+    uefi-run
+    lxc
+    swtpm
+  ])
+}
